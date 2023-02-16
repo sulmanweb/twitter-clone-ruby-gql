1149d4fba5008654552bf396a6c971def561a47a
Sulman Baig <sulman@hey.com>
2023-02-16 21:31:05 +0500

    Create User Model #1 - Adding ActiveStorage and bundler update

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module TwitterCloneRubyGql
+  # @note: Application class
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
