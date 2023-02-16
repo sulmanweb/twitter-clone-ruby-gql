cad9679f917470e9fec9a9133682c61bf686ba64
Sulman Baig <sulman@hey.com>
2023-02-16 22:31:05 +0500

    Create User Model #1 - Adding User Model

---
+++ app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  bio             :string
+#  email           :string
+#  location        :string
+#  name            :string
+#  password_digest :string
+#  username        :string
+#  website         :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+class User < ApplicationRecord
+  # @note: Entities
+  has_secure_password
+  has_one_attached :profile_picture
+
+  # @note: Validations
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 },
+                       format: { with: /\A[a-z0-9_.]+\z/, message: I18n.t('models.user.username_format') }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: { with: /\A([\w+-].?)+@[a-z\d-]+(\.[a-z]+)*\.[a-z]+\z/i,
+                              message: I18n.t('models.user.email_format') }
+  validates :password, length: { minimum: 8, maximum: 72 },
+                       format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}\z/,
+                                 message: I18n.t('models.user.password_format') }, if: :password_required?
+  validates :name, length: { maximum: 50 }, allow_blank: true
+  validates :bio, length: { maximum: 200 }, allow_blank: true
+  validates :location, length: { maximum: 60 }, allow_blank: true
+  validates :website,
+            length: { maximum: 160 },
+            format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: I18n.t('models.user.website_format') }, allow_blank: true
+
+  private
+
+  # @note: This method is used to validate the password only when it is present or when the user is being created.
+  # @return [Boolean]
+  def password_required?
+    new_record? || password.present?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_162833) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_163417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_162833) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
+    t.string "bio"
+    t.string "location"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
