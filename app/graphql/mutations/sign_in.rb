0643e2de67971a7142466b904e73e1ec6e6365ab
Sulman Baig <sulman@hey.com>
2023-02-17 22:09:20 +0500

    Create a mutation for SignIn #7 - SignIn Mutation (#19)

---
+++ app/graphql/mutations/sign_in.rb
@@ -0,0 +1,22 @@
+module Mutations
+  # @note: This is the mutation that will be used to sign in a user.
+  class SignIn < Mutations::BaseMutation
+    description 'Sign in a user'
+
+    argument :password, String, required: true, description: 'The password of the user'
+    argument :username, String, required: true, description: 'The username of the user'
+
+    field :auth_token, String, null: true, description: 'The auth token of the user'
+    field :errors, [String], null: true, description: 'The errors of the mutation'
+    field :user, Types::Objects::UserType, null: true, description: 'The user'
+
+    def resolve(username:, password:)
+      result = Users::SigninService.call(username:, password:)
+      {
+        auth_token: result.auth_token,
+        errors: result.errors,
+        user: result.user
+      }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
   class MutationType < Types::BaseObject
     description 'The mutation root of this schema'
 
+    field :sign_in, mutation: Mutations::SignIn, description: 'Sign in a user'
     field :sign_up, mutation: Mutations::SignUp, description: 'Sign up a new user'
   end
 end
