dd561cb956ad087480c0fa5d58d0306382f06f74
Sulman Baig <sulman@hey.com>
2023-03-19 16:11:10 +0500

    sulmanhey-lin-25 - Adding Likes To TweetType

--- app/graphql/types/objects/tweet_type.rb
+++ app/graphql/types/objects/tweet_type.rb
@@ -14,12 +14,23 @@ module Types
       field :retweet_count, Integer, null: false, description: 'The number of retweets of the tweet'
       field :retweets, Types::Objects::RetweetType.connection_type, null: true, description: 'The retweets of the tweet'
 
+      field :likes, Types::Objects::LikeType.connection_type, null: true, description: 'The likes of the tweet'
+      field :likes_count, Integer, null: false, description: 'The number of likes of the tweet'
+
       def retweet_count
         object.retweets.count
       end
 
       def retweets
         object.retweets.order(id: :desc)
+      end
+
+      def likes_count
+        object.likes.count
+      end
+
+      def likes
+        object.likes.order(id: :desc)
       end
     end
   end
